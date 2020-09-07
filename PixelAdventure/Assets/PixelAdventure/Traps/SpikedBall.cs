af6b6fc557beb48411883fd24151998c84594df4
TheAncientOwl <dele.alex.o.o@gmail.com>
2020-09-08 00:36:44 +0300

    Updated SpikedBall

--- PixelAdventure/Assets/PixelAdventure/Traps/SpikedBall.cs
+++ PixelAdventure/Assets/PixelAdventure/Traps/SpikedBall.cs
@@ -1,40 +1,94 @@
 ﻿using UnityEngine;
+using System;
 
 namespace PixelAdventure.Traps
 {
     public class SpikedBall : MonoBehaviour
     {
-        [SerializeField] Transform m_RotationCenter = null;
-        [SerializeField] Transform m_Collider = null;
-        [SerializeField] Transform m_Chain = null;
+        [SerializeField] RotationType m_RotationType = RotationType.Degrees360;
+        [SerializeField] Transform m_Center = null;
+        [SerializeField] Transform m_Ball = null;
         [SerializeField] float m_RotationRadius = 2f;
         [SerializeField] float m_AngularSpeed = -2f;
-        [SerializeField] float m_ChainAngularSpeed = -2f;
 
         private float m_Angle = 0f;
-        private float m_ChainAngle = 0f;
 
-        private void Update() 
+        private Action m_RotateFunction;
+
+        private void Start() 
         {
-            m_Collider.position = new Vector3
+            switch (m_RotationType)
+            {
+                case RotationType.Degrees360:
+                    m_RotateFunction = Rotate360Degrees;
+                    break;
+                case RotationType.Degrees180:
+                    m_RotateFunction = RotateDegrees180;
+                    break;
+                default:
+                    Debug.LogError("Unsupported type SpikedBall.RotationType");
+                    break;
+            }
+        }
+
+        private void Update() => m_RotateFunction();
+
+        private void Rotate360Degrees()
+        {
+            m_Ball.position = new Vector3
             (
-                x: m_RotationCenter.position.x + Mathf.Cos(m_Angle) * m_RotationRadius,
-                y: m_RotationCenter.position.y + Mathf.Sin(m_Angle) * m_RotationRadius,
-                z: m_Collider.position.z
+                x: m_Center.position.x + Mathf.Cos(m_Angle) * m_RotationRadius,
+                y: m_Center.position.y + Mathf.Sin(m_Angle) * m_RotationRadius,
+                z: m_Ball.position.z
             );
 
             m_Angle += Time.deltaTime * m_AngularSpeed;
             
-            if (m_Angle >= 360f)
+            if (m_Angle >= 360f) 
                 m_Angle = 0f;
 
+            Vector2 direction = m_Ball.position - m_Center.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            m_Center.rotation = rotation;
+        }
 
-            m_RotationCenter.eulerAngles = new Vector3(0f, 0f, m_ChainAngle);
+        private void RotateDegrees180()
+        {
+            //if (!Input.GetKeyDown(KeyCode.S)) return;
+            if (m_Ball.position.y >= m_Center.position.y + 0.1f)
+            {
+                m_AngularSpeed = -m_AngularSpeed;
+            }
 
-            m_ChainAngle += Time.deltaTime * m_ChainAngularSpeed;
+            m_Angle += Time.deltaTime * m_AngularSpeed;
 
-            if (m_ChainAngle >= 360f)
-                m_ChainAngle = 0f;
+            m_Ball.position = new Vector3
+            (
+                x: m_Center.position.x + Mathf.Cos(m_Angle) * m_RotationRadius,
+                y: m_Center.position.y + Mathf.Sin(m_Angle) * m_RotationRadius,
+                z: m_Ball.position.z
+            );
+
+
+            if (m_Angle <= -180f)
+                m_Angle = 0f;
+
+            // Debug.Log(m_Angle);
+
+            Vector2 direction = m_Ball.position - m_Center.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            m_Center.rotation = rotation;
+
+            
+                
+        }
+
+        private enum RotationType
+        {
+            Degrees360,
+            Degrees180
         }
 
     }
